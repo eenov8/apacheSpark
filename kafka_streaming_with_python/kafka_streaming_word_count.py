e3ae573b4a5d3763927de5798027ddfbcd1c8a50
Anurag Jain <anuragjain@bhavnacorp.com>
2015-09-14 09:11:44 +0530

    Updated kafka streaming python file with proper comments.

--- kafka_streaming_with_python/kafka_streaming_word_count.py
+++ kafka_streaming_with_python/kafka_streaming_word_count.py
@@ -1,5 +1,8 @@
 # Download the required Jar from https://drive.google.com/file/d/0B70R5CJksVCnTTNLc3pxaENZWjg/view?usp=sharing
-#bin/spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.0 --jars /home/edureka/Desktop/downloads/spark-streaming-kafka-assembly_2.10-1.5.0.jar /home/anurag/Desktop/spark/kafka_spark_wc.py localhost:9092 test
+
+# bin/spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.0 
+# --jars /home/edureka/Desktop/downloads/spark-streaming-kafka-assembly_2.10-1.5.0.jar 
+# /home/anurag/Desktop/spark/kafka_spark_wc.py localhost:9092 test
 
 import sys
 
@@ -12,16 +15,32 @@ if __name__ == "__main__":
         print("Usage: kafka_spark_wc.py <broker_list> <topic>")
         exit(-1)
 
-    sc = SparkContext(appName="PythonKafkaWordCount")
+    #create SparkContext which will run on local machine as master
+	#since one thread will be listening to the incoming stream, it will be a good idea to have at least two threads,
+	#one to listen to the stream and another one that will process the same.
+	sc = SparkContext(appName="PythonKafkaWordCount")
+	
+	#Create a streaming Spark Context that will poll the source every 2 seconds to create a DStream.
     ssc = StreamingContext(sc, 2)
 
+	#read the command line argument to create the brokers list and the topic that we need to listen to.
+	#If we want to listen multiple topics we need to create multiple streams for the same as in next line.
     brokers, topic = sys.argv[1:]
+	
+	#Create a Dstream from kafkaUtils library that comes along with the included jar.
+	#Create multiple such streams if we have more that one topic to be read.
     kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+	
+	#Now comes in the standard code that will read the line, split it on spaces to get the words and then finally reduce by the keys
+	#to generate the word count.
     lines = kvs.map(lambda x: x[1])
     counts = lines.flatMap(lambda line: line.split(" ")) \
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
+    
+	#pprint will print the first 10 entries.
+	counts.pprint()
 
+	#Start the streaming context and await manual interruption.
     ssc.start()
     ssc.awaitTermination()
